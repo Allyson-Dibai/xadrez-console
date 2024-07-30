7de8c9c7fd457c451c1d9246253ef24203c028f7
Allyson Dibai <allysondibai@outlook.com>
2024-07-30 11:17:16 -0300

    Comecando a implementar o tabuleiro

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -7,8 +7,9 @@ namespace XadrezConsole
     {
         static void Main(string[] args)
         {
-            Posicao p = new Posicao(3, 4);
-            Console.WriteLine("Posição " + p);
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Console.ReadLine();
         }
     }
 }
